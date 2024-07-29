6d34f145ba91224a0e8427ad83deab5ee6f1b9e3
Turito Yuenan <74642745+TuritoYuenan@users.noreply.github.com>
2024-07-29 15:28:32 +0700

    attempt to implement Builder pattern; utilise MaterialBuilder in AddPage

---
+++ Models/MaterialBuilder.cs
@@ -0,0 +1,110 @@
+﻿using System.Text.RegularExpressions;
+
+namespace PersonalLibrary.Models;
+
+/// <summary>
+/// Provides extension methods to build Material items, with custom data validation
+/// </summary>
+/// <remarks>Implements the Builder design pattern</remarks>
+/// <see href="https://www.webdevtutor.net/blog/c-sharp-builder-pattern-extension-methods"/>
+public static partial class MaterialBuilder
+{
+	public static Material AddAuthors(this Material material, string[] authors)
+	{
+		material.Authors = [.. authors];
+		return material;
+	}
+
+	public static Material AddTitle(this Material material, string title)
+	{
+		material.Title = title;
+		return material;
+	}
+
+	public static Material AddDate(this Material material, int year, int month = 1, int day = 1)
+	{
+		if (1 > month || month > 12)
+		{
+			throw new ArgumentOutOfRangeException(nameof(month), "Month must be between 1 and 12");
+		}
+		if (1 > day || day > 31)
+		{
+			throw new ArgumentOutOfRangeException(nameof(day), "Day must be between 1 and 31");
+		}
+		material.Date = new(year, month, day);
+		return material;
+	}
+
+	public static Material AddPublication(this Material material, string publication)
+	{
+		if (material is Book book) book.Publisher = publication;
+		if (material is Article article) article.Publisher = publication;
+		if (material is Webpage webpage) webpage.Website = publication;
+		if (material is YouTubeVideo video) video.Platform = publication;
+		return material;
+	}
+
+	public static Book AddEdition(this Book book, string edition)
+	{
+		book.Edition = edition;
+		return book;
+	}
+
+	public static Book AddIsbn(this Book book, string isbn)
+	{
+		if (!IsbnPattern().IsMatch(isbn))
+		{
+			throw new ArgumentException("The given ISBN is not valid", nameof(isbn));
+		}
+		book.Isbn = isbn;
+		return book;
+	}
+
+	public static Article AddNumbers(this Article article, int volume = -1, int issue = -1, int start = -1, int end = -1)
+	{
+		article.Numbers = (volume, issue, start, end);
+		return article;
+	}
+
+	public static Article AddDoi(this Article article, string doi)
+	{
+		if (!DoiPattern().IsMatch(doi))
+		{
+			throw new ArgumentException("The given DOI is not valid", nameof(doi));
+		}
+		article.Doi = doi;
+		return article;
+	}
+
+	public static Webpage AddLink(this Webpage webpage, string url)
+	{
+		if (!UrlPattern().IsMatch(url))
+		{
+			throw new ArgumentException("The given URL is not valid");
+		}
+		webpage.Link = new(url);
+		return webpage;
+	}
+
+	public static YouTubeVideo AddLink(this YouTubeVideo video, string url)
+	{
+		if (!UrlPattern().IsMatch(url))
+		{
+			throw new ArgumentException("The given URL is not valid");
+		}
+		video.Link = new(url);
+		return video;
+	}
+
+	// https://www.geeksforgeeks.org/regular-expressions-to-validate-isbn-code
+	[GeneratedRegex(@"^(?=(?:[^0-9]*[0-9]){10}(?:(?:[^0-9]*[0-9]){3})?$)[\d-]+$")]
+	private static partial Regex IsbnPattern();
+
+	// https://www.crossref.org/blog/dois-and-matching-regular-expressions
+	[GeneratedRegex(@"^10.\d{4,9}/[-._;()/:A-Z0-9]+$")]
+	private static partial Regex DoiPattern();
+
+	// https://urlregex.com
+	[GeneratedRegex(@"^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&%\$#_]*)?$")]
+	private static partial Regex UrlPattern();
+}
--- Views/AddPage.cs
+++ Views/AddPage.cs
@@ -1,57 +1,134 @@
 ﻿using PersonalLibrary.Interfaces;
+using PersonalLibrary.Models;
 using SplashKitSDK;
 
 namespace PersonalLibrary.Views;
 
 public class AddPage : IPage
 {
-	private readonly Dictionary<string, object> _form;
+	private Dictionary<string, bool> _buttons;
+	private int _type;
+	private (string A, string T, string P, string I) _common;
+	private (int Y, int M, int D) _date;
+	private (int V, int I, int S, int E) _article;
+	private string _bEdition;
 
-	public string Title => "Add new material";
+	public string Title => "Add New " + TypeLabel();
 
 	public AddPage()
 	{
-		_form = [];
-		_form["authors"] = "";
-		_form["title"] = "";
-		_form["publisher"] = "";
-		_form["type"] = 0;
-		_form["year"] = 2000;
-		_form["month"] = 12;
-		_form["day"] = 1;
-		_form["id"] = "";
+		_buttons = UserInterface.GetInstance().Buttons;
+		_type = 0;
+		_common = ("", "", "", "");
+		_date = (2000, 1, 1);
+		_bEdition = "";
+		_article = (-1, -1, -1, -1);
 	}
 
 	public void Render()
 	{
-		SplashKit.SetInterfaceLabelWidth(100);
 		SplashKit.SetInterfaceFontSize(20);
 		SplashKit.StartCustomLayout();
 
-		SplashKit.StartInset("type", SplashKit.RectangleFrom(20, 110, 900, 45));
-		_form["type"] = (int)SplashKit.Slider("Type", (int)_form["type"], 0, 4);
-		SplashKit.EndInset("type");
+		SplashKit.SetInterfaceLabelWidth(200);
+		SplashKit.StartInset("Type", SplashKit.RectangleFrom(320, 145, 600, 45));
+		_type = (int)SplashKit.Slider("Type: " + TypeLabel(), _type, 0, 3);
+		SplashKit.EndInset("Type");
 
-		TextField(20, 155, "Author(s)", "authors");
-		TextField(20, 200, "Title", "title");
-		TextField(20, 245, "Publisher", "publisher");
-		TextField(20, 290, "ID", "id");
-		NumberField(20, 335, "Year", "year");
-		NumberField(320, 335, "Month", "month");
-		NumberField(620, 335, "Day", "day");
+		SplashKit.SetInterfaceLabelWidth(100);
+		_common.A = TextField(320, 190, "Author(s)", _common.A);
+		_common.T = TextField(320, 235, "Title", _common.T);
+		_date.Y = NumberField(320, 280, "Year", _date.Y);
+		_date.M = NumberField(520, 280, "Month", _date.M);
+		_date.D = NumberField(720, 280, "Day", _date.D);
+
+		if (_type == 0)
+		{
+			_common.P = TextField(320, 325, "Publisher", _common.P);
+			_bEdition = TextField(320, 370, "Edition", _bEdition);
+			_common.I = TextField(320, 415, "ISBN", _common.I);
+		}
+
+		if (_type == 1)
+		{
+			_common.P = TextField(320, 325, "Publisher", _common.P);
+			_article.V = NumberField(420, 415, "Volume", _article.V);
+			_article.I = NumberField(620, 415, "Issue", _article.I);
+			_article.S = NumberField(420, 460, "Page", _article.S);
+			_article.E = NumberField(620, 460, "Page", _article.E);
+			_common.I = TextField(320, 370, "DOI", _common.I);
+		}
+
+		if (_type == 2 || _type == 3)
+		{
+			_common.P = TextField(320, 325, "Website", _common.P);
+			_common.I = TextField(320, 370, "URL", _common.I);
+		}
+
+		_buttons["create"] = SplashKit.Button("Add to Shelf", SplashKit.RectangleFrom(520, 550, 200, 100));
+		if (_buttons["create"]) BuildMaterial();
 	}
 
-	private void TextField(int x, int y, string label, string key)
+	private static string TextField(int x, int y, string label, string value)
 	{
-		SplashKit.StartInset(key, SplashKit.RectangleFrom(x, y, 900, 45));
-		_form[key] = SplashKit.TextBox(label, (string)_form[key]);
-		SplashKit.EndInset(key);
+		SplashKit.StartInset(label, SplashKit.RectangleFrom(x, y, 600, 45));
+		value = SplashKit.TextBox(label, value);
+		SplashKit.EndInset(label);
+		return value;
 	}
 
-	private void NumberField(int x, int y, string label, string key)
+	private static int NumberField(int x, int y, string label, int value)
 	{
-		SplashKit.StartInset(key, SplashKit.RectangleFrom(x, y, 300, 45));
-		_form[key] = (int)SplashKit.NumberBox(label, (int)_form[key], 1);
-		SplashKit.EndInset(key);
+		SplashKit.StartInset(label, SplashKit.RectangleFrom(x, y, 200, 45));
+		value = (int)SplashKit.NumberBox(label, value, 1);
+		SplashKit.EndInset(label);
+		return value;
+	}
+
+	private string TypeLabel()
+	{
+		return _type switch
+		{
+			0 => "Book",
+			1 => "Article",
+			2 => "Webpage",
+			3 => "Video",
+			_ => "Material"
+		};
+	}
+
+	private Material BuildMaterial()
+	{
+		var authors = _common.A.Split([',', '/']);
+		return _type switch
+		{
+			0 => new Book()
+				.AddIsbn(_common.I)
+				.AddEdition(_bEdition)
+				.AddPublication(_common.P)
+				.AddDate(_date.Y, _date.M, _date.D)
+				.AddTitle(_common.T)
+				.AddAuthors(authors),
+			1 => new Article()
+				.AddDoi(_common.I)
+				.AddNumbers(_article.V, _article.I, _article.S, _article.E)
+				.AddPublication(_common.P)
+				.AddDate(_date.Y, _date.M, _date.D)
+				.AddTitle(_common.T)
+				.AddAuthors(authors),
+			2 => new Webpage()
+				.AddLink(_common.I)
+				.AddPublication(_common.P)
+				.AddDate(_date.Y, _date.M, _date.D)
+				.AddTitle(_common.T)
+				.AddAuthors(authors),
+			3 => new YouTubeVideo()
+				.AddLink(_common.I)
+				.AddPublication(_common.P)
+				.AddDate(_date.Y, _date.M, _date.D)
+				.AddTitle(_common.T)
+				.AddAuthors(authors),
+			_ => throw new NotSupportedException("Unknown type")
+		};
 	}
 }
